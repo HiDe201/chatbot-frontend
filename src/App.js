d9cb800645100cd9c8d3d606403e0b8e148d6345
HiDe201 <hiya.desai2001.com>
2025-04-04 00:10:33 -0400

    Initial commit for frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MedicalChatbot from "./Chatbot"; // Ensure this matches your file structure
+
+function App() {
+  return (
+    <div>
+      <MedicalChatbot />
+    </div>
+  );
+}
+
+export default App;
